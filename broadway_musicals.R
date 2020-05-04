9edbf00fff3895ea26a71525cd40af1ce52ac525
Martín Pons <martinpons10218@gmail.com>
2020-05-04 22:24:24 +0200

    cambio comentario de adjust_revenue_variable

--- broadway_musicals.R
+++ broadway_musicals.R
@@ -19,7 +19,7 @@ library(here)
 
 # helpler functions
 
-# adjust_variable is used to deflate revenue variables, taking inflation into account
+# adjust_revenue_variable is used to deflate revenue variables, taking inflation into account
 adjust_revenue_variable <- function(var, deflator) {
   
   # numeric, numeric -> numeric
